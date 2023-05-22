820462c1e9dfc8d3a89369d69da64733c52d4002
Alexey <131549768+BerkovskiiAlex@users.noreply.github.com>
2023-05-22 14:09:53 +0300

    Initial commit

---
+++ js/modal.js
@@ -0,0 +1,22 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    backdrop: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  refs.backdrop.addEventListener("click", logBackdropClick);
+
+  function toggleModal() {
+    refs.backdrop.classList.toggle("is-hidden");
+  }
+
+  function logBackdropClick(e) {
+    if (e.target.className !== "backdrop") return;
+
+    refs.backdrop.classList.toggle("is-hidden");
+  }
+})();
