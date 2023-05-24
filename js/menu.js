695b19db5e91a9ef7b619cc4dbc72e3b6cdd54f9
BerkovskiiAlex <berkovskii845@gmail.com>
2023-05-24 14:06:12 +0300

    обнова перед сначей на проверку

--- js/menu.js
+++ js/menu.js
@@ -5,6 +5,8 @@
     menu: document.querySelector('[data-menu]'),
   };
 
+    
+    
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
 
